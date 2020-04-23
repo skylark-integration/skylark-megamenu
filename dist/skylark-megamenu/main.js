974a0b2a0b3ad8f1606f76cfcd2388277b17e191
hudaokeji <github@hudaokeji.com>
2020-04-23 16:57:32 +0900

    init commit

---
+++ dist/skylark-megamenu/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-megamenu - A version of megamenu that ported to running on skylarkjs ui.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-megamenu/
+ * @license MIT
+ */
+define(["./megamenu"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-megamenu/main.js
@@ -0,0 +1,5 @@
+define([
+	"./megamenu"
+],function(megamenu){
+	return megamenu;
+});
